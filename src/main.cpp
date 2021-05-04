f8342886e95f3a70047efbd1da0953ff7e575af4
cmdc0de <cmdc0dez@gmail.com>
2021-05-04 01:08:58 -0700

    builds and can run while linked to wasm

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <spdlog/spdlog.h>
 #include <docopt/docopt.h>
+#include <wasm_export.h>
 
 static constexpr auto USAGE =
   R"(Naval Fate.
@@ -22,8 +23,8 @@ static constexpr auto USAGE =
           --drifting    Drifting mine.
 )";
 
-int main(int argc, const char **argv)
-{
+int main(int argc, const char **argv) {
+/*
   std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
     { std::next(argv), std::next(argv, argc) },
     true,// show help if requested
@@ -33,9 +34,34 @@ int main(int argc, const char **argv)
     std::cout << arg.first << arg.second << std::endl;
   }
 
-
+*/
+  std::ignore = argc;
+  std::ignore = argv;
   //Use the default logger (stdout, multi-threaded, colored)
   spdlog::info("Hello, {}!", "World");
 
   fmt::print("Hello, from {}\n", "{fmt}");
+
+  //char *buffer, error_buf[128];
+  //wasm_module_t module;
+  //wasm_module_inst_t module_inst;
+  //wasm_function_inst_t func;
+  //wasm_exec_env_t exec_env;
+  //uint32 size, stack_size = 8092, heap_size = 8092;
+
+  /* initialize the wasm runtime by default configurations */
+  wasm_runtime_init();
+
+  spdlog::info("WASM Runtime INIT");
+  /* read WASM file into a memory buffer */
+  //buffer = read_wasm_binary_to_buffer(…, &size);
+
+  /* add line below if we want to export native functions to WASM app */
+  //wasm_runtime_register_natives(...);
+
+  /* parse the WASM file from buffer and create a WASM module */
+  //module = wasm_runtime_load(buffer, size, error_buf, sizeof(error_buf));
+
+  /* create an instance of the WASM module (WASM linear memory is ready) */
+  //module_inst = wasm_runtime_instantiate(module, stack_size, heap_size,
 }
